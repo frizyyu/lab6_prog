b9447e3f3cfb6315df02982a100290b0fceb4011
Artem <84192047+frizyyu@users.noreply.github.com>
2024-03-25 23:56:48 +0300

    Add files via upload

---
+++ client/src/main/java/validators/ClearValidator.java
@@ -0,0 +1,31 @@
+package validators;
+
+import helpers.*;
+
+import java.util.Objects;
+
+public class ClearValidator implements ValidatorInterface{
+    SimpleValidator valid = new SimpleValidator();
+    @Override
+    public boolean validationCheck(String[] str) {
+        if (this.valid.validate(str)){
+            System.out.print("Are you sure about deleting the collection? y/n\n>>> ");
+            ContinueAction cont = new ContinueAction();
+            int c = cont.continueAction("Clear collection..", "The collection has not been cleared", "Action skipped. Invalid answer");
+            if (c == 1){
+                return true;
+            } else if (c == -1) {
+                return false;
+            }
+            else
+                return false;
+        }
+        else
+            return false;
+    }
+
+    @Override
+    public String parameterCheck(String[] str) {
+        return this.valid.paramCheck(str);
+    }
+}
